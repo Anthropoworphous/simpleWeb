fd590ef5db831f8037c78f675496432837ce48a3
Anthropoworphous <kevin1050914@gmail.com>
2022-05-14 17:10:48 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static(path.resolve("assets")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("assets", "html", "e.html"));
+});
+
+app.listen(8080);
